e0c051218f025209545e19c483d9ba7a5788574e
Ken Sodemann <ken@ionic.io>
2019-12-09 15:40:59 -0600

    chore(app): upgrade to Angular 8 and latest Ionic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'white', pathMatch: 'full' },
-  { path: 'white', loadChildren: './white/white.module#WhitePageModule' },
-  { path: 'yellow', loadChildren: './yellow/yellow.module#YellowPageModule' },
-  { path: 'green', loadChildren: './green/green.module#GreenPageModule' },
-  { path: 'oolong', loadChildren: './oolong/oolong.module#OolongPageModule' },
-  { path: 'black', loadChildren: './black/black.module#BlackPageModule' },
-  { path: 'dark', loadChildren: './dark/dark.module#DarkPageModule' },
-  { path: 'puer', loadChildren: './puer/puer.module#PuerPageModule' },
-  { path: 'herbal', loadChildren: './herbal/herbal.module#HerbalPageModule' },
+  { path: 'white', loadChildren: () => import('./white/white.module').then(m => m.WhitePageModule) },
+  { path: 'yellow', loadChildren: () => import('./yellow/yellow.module').then(m => m.YellowPageModule) },
+  { path: 'green', loadChildren: () => import('./green/green.module').then(m => m.GreenPageModule) },
+  { path: 'oolong', loadChildren: () => import('./oolong/oolong.module').then(m => m.OolongPageModule) },
+  { path: 'black', loadChildren: () => import('./black/black.module').then(m => m.BlackPageModule) },
+  { path: 'dark', loadChildren: () => import('./dark/dark.module').then(m => m.DarkPageModule) },
+  { path: 'puer', loadChildren: () => import('./puer/puer.module').then(m => m.PuerPageModule) },
+  { path: 'herbal', loadChildren: () => import('./herbal/herbal.module').then(m => m.HerbalPageModule) },
 ];
 
 @NgModule({
